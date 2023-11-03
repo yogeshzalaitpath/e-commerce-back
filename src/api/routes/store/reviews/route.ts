45a00d736e5c107b459f9a4dff94034de8b14655
Gaurav Patel <gp627853@gmail.com>
2023-11-03 15:42:50 +0530

    Changes in get api

--- src/api/routes/store/reviews/route.ts
+++ src/api/routes/store/reviews/route.ts
@@ -1,33 +1,66 @@
-import type { MedusaRequest, MedusaResponse } from "@medusajs/medusa";
+import { Customer, type MedusaRequest, type MedusaResponse } from "@medusajs/medusa";
 import { ProductReview } from "../../../../models/product-review";
-import { EntityManager } from "typeorm";
+import { EntityManager, MoreThan } from "typeorm";
+import ImageRepository from "@medusajs/medusa/dist/repositories/image";
+import { FileService } from "medusa-interfaces";
 
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
   const manager: EntityManager = req.scope.resolve("manager");
   const productReviewRepo = manager.getRepository(ProductReview);
 
+  const [productReviews, counts] = await Promise.all([
+    productReviewRepo.find(), // Fetch all data
+    productReviewRepo
+      .createQueryBuilder('product_review')
+      .select([
+        'COUNT(CASE WHEN product_review.rating = 1 THEN 1 ELSE NULL END) AS count1',
+        'COUNT(CASE WHEN product_review.rating = 2 THEN 1 ELSE NULL END) AS count2',
+        'COUNT(CASE WHEN product_review.rating = 3 THEN 1 ELSE NULL END) AS count3',
+        'COUNT(CASE WHEN product_review.rating = 4 THEN 1 ELSE NULL END) AS count4',
+        'COUNT(CASE WHEN product_review.rating = 5 THEN 1 ELSE NULL END) AS count5',
+      ])
+      .getRawOne(),
+  ]);
+  
+  const totalCount = productReviews.length
+
   return res.json({
-    product_reviews: await productReviewRepo.find(),
+    product_reviews: productReviews,
+    totalCount: totalCount,
+    RatingCounts: counts,
   });
 };
+
 
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
   const manager: EntityManager = req.scope.resolve("manager");
   const productReviewRepo = manager.getRepository(ProductReview);
+  const customerRepo = manager.getRepository(Customer)
+  const imageRepo = manager.withRepository(ImageRepository)
 
-  
-  
-  const { title, description, rating, product_id, customer_id } = req.body
-
+  const { title, description, rating, product_id, customer_id, images } = req.body
+  const newImage = await imageRepo.upsertImages([images])
+  const data = await customerRepo.findOne({
+    where: {
+      id: customer_id,
+    },
+  })
+  const username = data.first_name + " " + data.last_name
+ 
   const newProductReview = new ProductReview();
   newProductReview.title = title;
   newProductReview.description = description;
   newProductReview.rating = rating;
   newProductReview.product = product_id;
   newProductReview.customer = customer_id;
-  console.log('req.body', req.body)
+  newProductReview.images = newImage;
+
+
+ 
   const finalData = await productReviewRepo.save(newProductReview)
+
   return res.json({
-    product_review: finalData
+    product_review: finalData,
+    username: username
   });
 };
